d6baea11d85609b09bdb8817b475723ef0f877c3
Илья Веселов <ves.ilya@mail.ru>
2021-01-08 21:48:15 +0300

    Added CMYK model

--- PoissonBlending.ConsoleApp/Program.cs
+++ PoissonBlending.ConsoleApp/Program.cs
@@ -11,7 +11,8 @@ namespace PoissonBlending.ConsoleApp
             var solver = new PoissonBlendingSolver((string message) => Console.WriteLine(message));
             //solver.Impose("A.jpg", "B.jpg", 300, 70, resultImageFilename: "rgbResult.jpg");
             //solver.Impose<HslPixel>("A.jpg", "B.jpg", 300, 70, resultImageFilename: "hslResult.jpg");
-            solver.Impose<CmyPixel>("A.jpg", "B.jpg", 300, 70, resultImageFilename: "cmyResult.jpg");
+            //solver.Impose<CmyPixel>("A.jpg", "B.jpg", 300, 70, resultImageFilename: "cmyResult.jpg");
+            solver.Impose<CmykPixel>("A.jpg", "B.jpg", 300, 70, resultImageFilename: "cmykResult.jpg");
             solver.ImposeWithoutBlending("A.jpg", "B.jpg", 300, 70, resultImageFilename: "resultWithoutBlending.jpg");
         }
     }
---
+++ PoissonBlending.Lib/PixelDescription/CmykPixel.cs
@@ -0,0 +1,122 @@
+﻿using Skybrud.Colors;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace PoissonBlending.Lib.PixelDescription
+{
+    public class CmykPixel : BasePixel
+    {
+        private static readonly List<string> ColorComponentsNames = new() { nameof(C), nameof(M), nameof(Y), nameof(K) };
+
+        public double C { get; set; }
+        public double M { get; set; }
+        public double Y { get; set; }
+        public double K { get; set; }
+
+        public CmykPixel()
+        {
+            C = M = Y = K = 0;
+        }
+
+        public CmykPixel(Color color)
+        {
+            var cmyColor = new CmyColor(color.R, color.G, color.B).ToCmyk();
+            C = cmyColor.C;
+            M = cmyColor.M;
+            Y = cmyColor.Y;
+            K = cmyColor.K;
+        }
+
+        public override double this[string colorComponentName]
+        {
+            get => colorComponentName switch
+            {
+                nameof(C) => C,
+                nameof(M) => M,
+                nameof(Y) => Y,
+                nameof(K) => K,
+                _ => throw new ArgumentException($"Unknown color component name {colorComponentName}")
+            };
+            set
+            {
+                switch (colorComponentName)
+                {
+                    case nameof(C):
+                        C = value;
+                        break;
+                    case nameof(M):
+                        M = value;
+                        break;
+                    case nameof(Y):
+                        Y = value;
+                        break;
+                    case nameof(K):
+                        K = value;
+                        break;
+                    default:
+                        throw new ArgumentException($"Unknown color component name {colorComponentName}");
+                }
+            }
+        }
+
+        public override CmykPixel FromColor(Color color)
+        {
+            var cmyColor = new RgbColor(color.R, color.G, color.B).ToCmyk();
+            C = cmyColor.C;
+            M = cmyColor.M;
+            Y = cmyColor.Y;
+            K = cmyColor.K;
+            return this;
+        }
+
+        public override Color ToColor()
+        {
+            var rgbColor = new CmykColor(GetColorComponentValue(C), GetColorComponentValue(M), GetColorComponentValue(Y), GetColorComponentValue(K)).ToRgb();
+            return Color.FromArgb(rgbColor.R, rgbColor.G, rgbColor.B);
+        }
+
+        public override List<string> GetColorComponentsNames() => ColorComponentsNames;
+
+        public override CmykPixel Multiply(int value)
+        {
+            foreach (var colorComponentsName in ColorComponentsNames)
+            {
+                this[colorComponentsName] *= value;
+            }
+            return this;
+        }
+
+        public override CmykPixel Add(BasePixel value)
+        {
+            var pixelValue = GetCmykPixel(value);
+            foreach (var colorComponentsName in ColorComponentsNames)
+            {
+                this[colorComponentsName] += pixelValue[colorComponentsName];
+            }
+            return this;
+        }
+
+        public override CmykPixel Minus(BasePixel value)
+        {
+            var pixelValue = GetCmykPixel(value);
+            foreach (var colorComponentsName in ColorComponentsNames)
+            {
+                this[colorComponentsName] -= pixelValue[colorComponentsName];
+            }
+            return this;
+        }
+
+        private static double GetColorComponentValue(double value) => value > 1 ? 1 : value < 0 ? 0 : value;
+
+        private static CmykPixel GetCmykPixel(BasePixel value)
+        {
+            if (value is not CmykPixel)
+            {
+                throw new ArgumentException($"Wrong argument type: {nameof(BasePixel)}. Expected type {nameof(CmykPixel)}.");
+            }
+
+            return value as CmykPixel;
+        }
+    }
+}
